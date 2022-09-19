e8dff7a9cbeece154c7a4eb8235e5dbe11896d42
khalidkhalil <kalidkalil4254@gmail.com>
2022-09-19 18:15:06 +0200

    monolith_app

---
+++ set_env.sh
@@ -0,0 +1,9 @@
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=postgres.cre5trd3yjkk.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET="udagram5165"
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=hello
+export URL=http://localhost:8080
---
+++ udagram-api/src/aws.ts
@@ -0,0 +1,36 @@
+import AWS = require('aws-sdk');
+import {config} from './config/config';
+
+let bucketName: string = config.aws_media_bucket;
+
+// Configure AWS
+const credentials = new AWS.SharedIniFileCredentials({profile: config.aws_profile});
+AWS.config.credentials = credentials;
+
+export const s3 = new AWS.S3({
+  signatureVersion: 'v4',
+  region: config.aws_region,
+  params: {Bucket: bucketName},
+});
+
+// Generates an AWS signed URL for retrieving objects
+export function getGetSignedUrl( key: string ): string {
+  const signedUrlExpireSeconds = 60 * 5;
+
+  return s3.getSignedUrl('getObject', {
+    Bucket: bucketName,
+    Key: key,
+    Expires: signedUrlExpireSeconds,
+  });
+}
+
+// Generates an AWS signed URL for uploading objects
+export function getPutSignedUrl( key: string ): string {
+  const signedUrlExpireSeconds = 60 * 5;
+
+  return s3.getSignedUrl('putObject', {
+    Bucket: bucketName,
+    Key: key,
+    Expires: signedUrlExpireSeconds,
+  });
+}
