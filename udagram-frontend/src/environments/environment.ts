cc8c06f24d9ae40dd4fa92dfdd076ec2bd0c9a20
khalidkhalil <kalidkalil4254@gmail.com>
2022-09-20 19:22:26 +0200

    add-nginx-endpoint-to-frontend

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a536df207e5ff4e01b0726b52284263b-1552124091.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
